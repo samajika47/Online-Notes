05a629917a6e949270eadf5f3b42df2401589114
samajika47 <samajikachakma47@gmail.com>
2024-09-25 11:13:38 +0600

    signup screen

---
+++ lib/signup_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SinupScreen extends StatelessWidget {
+  const SinupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
